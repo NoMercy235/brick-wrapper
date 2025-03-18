a704497315501f0b8d6867633cce6d11f49f3a7e
Alexandru Florian Barascu <alex.florin2352@gmail.com>
2025-03-18 11:59:25 +0200

    chore: fix typo

--- app/page.tsx
+++ app/page.tsx
@@ -137,7 +137,7 @@ export default function Home() {
       <div className="h-10"></div>
 
       <BrickWrapper configuration={guitaristBrickConfig} color="bg-red-400">
-        <p>Here is a random photo of the photo</p>
+        <p>Here is a random photo a guitarist</p>
         <Image
           alt="The guitarist in the concert."
           src="https://images.unsplash.com/photo-1464375117522-1311d6a5b81f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2250&q=80"
